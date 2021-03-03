c9c85fae114b4461314f9bd41c36d8eb4f508fef
Olaf-2137 <79452673+Olaf-2137@users.noreply.github.com>
2021-03-03 23:26:39 +0100

    Create Kot.java

---
+++ zadanie 1/Kot.java
@@ -0,0 +1,13 @@
+package com.company.z1;
+
+public class Kot extends Animal{
+
+
+    public Kot(String name, String color) {
+        super(name, color);
+    }
+
+    public void makeSound() {
+        System.out.println(name + " Kot sejt koloru " + color;
+    }
+}
