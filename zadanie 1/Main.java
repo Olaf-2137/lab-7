175f0e9d2aa6708eaa5c372e717e5526d292fe6f
Olaf-2137 <79452673+Olaf-2137@users.noreply.github.com>
2021-03-04 13:13:13 +0100

    Update Main.java

--- zadanie 1/Main.java
+++ zadanie 1/Main.java
@@ -4,11 +4,11 @@ package sample;
 public class main {
   //-----------------------------------------
     public static void main(String[] args) {
-        Pies pies = new Dog("Reks", "Szarego");
+        Pies pies = new Dog("tony", "Bronzowego");
         pies.makeSound();
 
       //-----------------------------------
-        Kot kot = new Cat("Puszek", "Czarnego");
+        Kot kot = new Cat("jamajka", "Szarego");
         kot.makeSound();
 
     }
